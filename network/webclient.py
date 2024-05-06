d25ff67ac89678df9c3f551989cb5827d79814bc
Fatescript <wangfeng19950315@163.com>
2024-05-06 13:55:05 +0800

    chore: add favicon of server

--- network/webclient.py
+++ network/webclient.py
@@ -5,17 +5,17 @@ import socket
 import webbrowser
 
 
-def connect(url: str, port: int = 80) -> str:
+def connect(url: str, port: int = 80, encoding: str = "ISO-8859-1") -> str:
+    # NOTE: To connect localhost, use 'localhost' as the url.
     s = socket.socket()
     s.connect((url, port))
     # If you were requesting a specific file, it would be on that first line, for example:
     # GET /path/to/file.html HTTP/1.1
-    # send_text = f"GET / HTTP/1.1\r\nHost: {url}\r\nConnection: close\r\n\r\n"
-    send_text = f"GET / HTTP/1.1\r\nHost: {url}\r\n\r\n"
+    send_text = f"GET / HTTP/1.1\r\nHost: {url}\r\nConnection: close\r\n\r\n"
     # if the data is not encoded with ISO-8859-1,
     # you’ll get weird characters in your string or an error.
     # For example.com, send_text.encode() also works.
-    s.sendall(send_text.encode("ISO-8859-1"))
+    s.sendall(send_text.encode(encoding=encoding))
 
     block_size = 4096
     text = ""
@@ -28,14 +28,16 @@ def connect(url: str, port: int = 80) -> str:
     return text
 
 
-def connect_and_display(url: str = "example.com", port: int = 80):
+def connect_and_display(url: str = "example.com", port: int = 80, display: bool = True):
     text = connect(url, port)
     header, web_text = text.rsplit("\r\n", maxsplit=1)
-    print(header)
+    print(f"Header:\n{header}\n")
+    print(f"Body:\n{web_text}")
     with open(f"{url}.html", "w") as f:
         f.write(web_text)
 
-    webbrowser.open("file://" + os.path.realpath(f"{url}.html"))
+    if display:
+        webbrowser.open("file://" + os.path.realpath(f"{url}.html"))
 
 
 if __name__ == "__main__":
--- network/webserver.py
+++ network/webserver.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
 
+import os
 import socket
 
 
-def server(port: int = 28333):
+def download_favicon(save_name: str = "favicon.ico") -> str:
+    import requests
+    if os.path.exists(save_name):
+        return save_name
+
+    url = "https://raw.githubusercontent.com/FateScript/FateScript.github.io/master/favicon.ico"
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(save_name, 'wb') as f:
+            f.write(response.content)
+            print("Download successfully!")
+    else:
+        print("Failed to download!")
+    return save_name
+
+
+def is_favicon(recv_header: str) -> bool:
+    return recv_header.split("\r\n")[0].split(" ")[1] == "/favicon.ico"
+
+
+def server(port: int = 28333, encoding: str = "ISO-8859-1"):
     s = socket.socket()
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(('', port))
@@ -11,11 +32,25 @@ def server(port: int = 28333):
     print(f"Server is runing on http://localhost:{port}")
     while True:
         new_socket, addr_info = s.accept()
-        print(f"Addr info: {addr_info}")
+        print(f"Addr info(ip, port): {addr_info}")
         data = new_socket.recv(4096)
-        print(f"Recv data:\n{data.decode('utf-8')}")
-        send_text = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>Hello world!</h1>"
-        new_socket.sendall(send_text.encode("ISO-8859-1"))
+        decoded_data = data.decode(encoding)
+        recv_header, *recv_body = decoded_data.split("\r\n\r\n", maxsplit=1)
+        print(f"Recv Header:\n{recv_header}\n")
+        if recv_body:
+            body_text = recv_body[0]
+            if body_text:
+                print(f"Recv Body:\n{body_text}\n")
+        # send_text = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>Hello world!</h1>"
+        if is_favicon(recv_header):
+            fav_file = download_favicon()
+            with open(fav_file, "rb") as f:
+                icon_data = f.read()
+            send_text = "HTTP/1.1 200 OK\r\nContent-Type: image/x-icon\r\n\r\n" + icon_data.decode(encoding)
+        else:
+            send_text = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello world!"
+
+        new_socket.sendall(send_text.encode(encoding=encoding))
         new_socket.close()
 
 
