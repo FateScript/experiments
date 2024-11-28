3f40e6937a5dcc5fec1c4b45bc3351222ecf184f
Fatescript <wangfeng19950315@163.com>
2024-11-28 17:39:40 +0800

    feat(nn/module): add mqa/gqa with kv cache

--- nn/module/mha.py
+++ nn/module/mha.py
@@ -8,7 +8,6 @@ from torch import nn
 
 
 class ScaledDotProductAttn(nn.Module):
-
     def __init__(self, return_attn=False):
         super().__init__()
         self.return_attn = return_attn
@@ -27,11 +26,20 @@ class ScaledDotProductAttn(nn.Module):
         return v
 
 
-class MultiHeadAttn(nn.Module):
+def split_heads(x, num_heads: int):
+    batch, seq, _ = x.shape
+    # split model_dim to (num_heads, tensor_dim)
+    x = x.reshape(batch, seq, num_heads, -1)
+    # transpose to (batch, num_heads, seq_len, tensor_dim)
+    x = x.transpose(1, 2)
+    return x
 
+
+class MultiHeadAttn(nn.Module):
     def __init__(self, dim: int, num_heads: int, use_kv_cache: bool = False):
         # NOTE: To visualize kv cache algorithm, try the following gif:
         # https://miro.medium.com/v2/resize:fit:1400/format:webp/1*uyuyOW1VBqmF5Gtv225XHQ.gif
+        assert dim % num_heads == 0, f"dim must be divisible by num_heads, got dim {dim} and head {num_heads}"  # noqa
         super().__init__()
         self.q_proj = nn.Linear(dim, dim, bias=False)
         self.k_proj = nn.Linear(dim, dim, bias=False)
@@ -58,26 +66,105 @@ class MultiHeadAttn(nn.Module):
             self.k_cache.append(prev_k)
             self.v_cache.append(prev_v)
 
-        q, k, v = self.split_heads(q), self.split_heads(k), self.split_heads(v)
+        h = self.num_heads
+        q, k, v = split_heads(q, h), split_heads(k, h), split_heads(v, h)
         out = self.attn(q, k, v, mask=mask)
         out = out.transpose(1, 2).reshape(batch, seq, dim)
         return self.out_proj(out)
 
-    def split_heads(self, x):
+
+class MultiQueryAttn(nn.Module):
+    def __init__(self, dim: int, num_heads: int, use_kv_cache: bool = False):
+        assert dim % num_heads == 0, f"dim must be divisible by num_heads, got dim {dim} and head {num_heads}"  # noqa
+        super().__init__()
+        head_dim = dim // num_heads
+        self.q_proj = nn.Linear(dim, dim, bias=False)
+        self.k_proj = nn.Linear(dim, head_dim, bias=False)
+        self.v_proj = nn.Linear(dim, head_dim, bias=False)
+        self.out_proj = nn.Linear(dim, dim, bias=False)
+
+        self.attn = ScaledDotProductAttn()
+        self.dim = dim
+        self.num_heads = num_heads
+
+        self.use_kv_cache = use_kv_cache
+        if self.use_kv_cache:
+            self.k_cache = []
+            self.v_cache = []
+
+    def forward(self, x, mask=None):
         batch, seq, dim = x.shape
-        # split model_dim to (num_heads, tensor_dim)
-        x = x.reshape(batch, seq, self.num_heads, -1)
-        # transpose to (batch, num_heads, seq_len, tensor_dim)
-        x = x.transpose(1, 2)
-        return x
+        q, k, v = self.q_proj(x), self.k_proj(x), self.v_proj(x)
+
+        if self.use_kv_cache:
+            prev_k, prev_v = k, v
+            k = torch.concat([*self.k_cache, prev_k], dim=1)
+            v = torch.concat([*self.v_cache, prev_v], dim=1)
+            self.k_cache.append(prev_k)
+            self.v_cache.append(prev_v)
+
+        q = split_heads(q, num_heads=self.num_heads)
+        k = k.unsqueeze(1).repeat(1, self.num_heads, 1, 1)
+        v = v.unsqueeze(1).repeat(1, self.num_heads, 1, 1)
+
+        out = self.attn(q, k, v, mask=mask)
+        out = out.transpose(1, 2).reshape(batch, seq, dim)
+        return self.out_proj(out)
 
 
-def save_input_output():
+class GroupQueryAttn(nn.Module):
+    def __init__(
+        self, dim: int, num_heads: int, num_groups: int, use_kv_cache: bool = False
+    ):
+        assert dim % num_heads == 0, f"dim must be divisible by num_heads, got dim {dim} and head {num_heads}"  # noqa
+        assert num_heads % num_groups == 0, f"num_heads must be divisible by num_groups, got head {num_heads} and group {num_groups}"  # noqa
+
+        super().__init__()
+        head_dim = dim // num_heads
+        group_dim = head_dim * num_groups
+
+        self.q_proj = nn.Linear(dim, dim, bias=False)
+        self.k_proj = nn.Linear(dim, group_dim, bias=False)
+        self.v_proj = nn.Linear(dim, group_dim, bias=False)
+        self.out_proj = nn.Linear(dim, dim, bias=False)
+
+        self.attn = ScaledDotProductAttn()
+        self.dim = dim
+        self.num_heads = num_heads
+        self.num_groups = num_groups
+        self.repeat_size = num_heads // num_groups
+
+        self.use_kv_cache = use_kv_cache
+        if self.use_kv_cache:
+            self.k_cache = []
+            self.v_cache = []
+
+    def forward(self, x, mask=None):
+        batch, seq, dim = x.shape
+        q, k, v = self.q_proj(x), self.k_proj(x), self.v_proj(x)
+
+        if self.use_kv_cache:
+            prev_k, prev_v = k, v
+            k = torch.concat([*self.k_cache, prev_k], dim=1)
+            v = torch.concat([*self.v_cache, prev_v], dim=1)
+            self.k_cache.append(prev_k)
+            self.v_cache.append(prev_v)
+
+        q = split_heads(q, self.num_heads)
+        k, v = split_heads(k, self.num_groups), split_heads(v, self.num_groups)
+        k = k.repeat(1, self.repeat_size, 1, 1)
+        v = v.repeat(1, self.repeat_size, 1, 1)
+
+        out = self.attn(q, k, v, mask=mask)
+        out = out.transpose(1, 2).reshape(batch, seq, dim)
+        return self.out_proj(out)
+
+
+def save_mha_input_output(batch: int = 2, seq: int = 16, dim: int = 32, heads: int = 4):
     # save input, output and model state dict
     # used for numpy version MHA in experiments/se/mpi
-    batch, seq, dim = 2, 16, 32
     x = torch.randn(batch, seq, dim)
-    model = MultiHeadAttn(dim, 4)
+    model = MultiHeadAttn(dim, heads)
     out = model(x)
     model_state = model.state_dict()
     data_dict = {"input": x, "output": out, "model_state": model_state}
@@ -87,20 +174,58 @@ def save_input_output():
     print(f"Dump data to {save_file}")
 
 
+def test_gqa_mha(batch: int = 2, seq: int = 16, dim: int = 32, heads: int = 4):
+    x = torch.randn(batch, seq, dim)
+    mha_model = MultiHeadAttn(dim, heads)
+    gqa_model = GroupQueryAttn(dim, heads, num_groups=heads)
+    gqa_model.load_state_dict(mha_model.state_dict())
+
+    mha_out = mha_model(x)
+    gqa_out = gqa_model(x)
+    assert torch.allclose(mha_out, gqa_out, atol=1e-7), "Check failed"
+    print("\nPassed GQA MHA test")
+
+
+def test_gqa_mqa(batch: int = 2, seq: int = 16, dim: int = 32, heads: int = 4):
+    x = torch.randn(batch, seq, dim)
+    mqa_model = MultiQueryAttn(dim, heads)
+    gqa_model = GroupQueryAttn(dim, heads, num_groups=1)
+    gqa_model.load_state_dict(mqa_model.state_dict())
+
+    mqa_out = mqa_model(x)
+    gqa_out = gqa_model(x)
+    assert torch.allclose(mqa_out, gqa_out, atol=1e-7), "Check failed"
+    print("\nPassed GQA MQA test")
+
+
 def check_kv_output_data(outputs, outputs_wo_kv):
     token_length = len(outputs)
     for idx in range(1, token_length + 1):
         data = torch.concat(outputs[:idx], dim=1)
-        assert torch.allclose(data, outputs_wo_kv[idx-1]), "Output not match"
+        assert torch.allclose(data, outputs_wo_kv[idx - 1], atol=1e-7), "Check failed"
     print("\nPassed kv cache test")
 
 
-def test_kv_cache():
-    batch, seq, dim = 2, 16, 32
+def test_kv_cache(
+    batch: int = 2,
+    seq: int = 16,
+    dim: int = 32,
+    num_heads: int = 4,
+    attn_type: str = "mha",
+):
+    assert attn_type in ["mha", "mqa", "gqa"]
+    kwargs = {}
+    if attn_type == "gqa":
+        attn_class = GroupQueryAttn
+        kwargs["num_groups"] = 2
+    else:
+        attn_class = MultiHeadAttn if attn_type == "mha" else MultiQueryAttn
+
     x = torch.randn(batch, seq, dim)
     x1, x2 = torch.randn(batch, 1, dim), torch.randn(batch, 1, dim)
     seq = [x, x1, x2]
-    model = MultiHeadAttn(dim, 4, use_kv_cache=True)
+
+    model = attn_class(dim, num_heads, use_kv_cache=True, **kwargs)
 
     print("Inference with kv cache")
     outputs = []
@@ -115,7 +240,7 @@ def test_kv_cache():
         print(f"Outpt shape with kv: {out.shape}")
 
     print("\nInference without kv cache")
-    model_wo_kv = MultiHeadAttn(dim, 4, use_kv_cache=False)
+    model_wo_kv = attn_class(dim, num_heads, use_kv_cache=False, **kwargs)
     model_wo_kv.load_state_dict(model.state_dict())
     outputs_wo_kv = []
     for idx in range(1, len(seq) + 1):
@@ -132,5 +257,10 @@ def test_kv_cache():
 
 if __name__ == "__main__":
     torch.manual_seed(3)
-    save_input_output()
-    test_kv_cache()
+    save_mha_input_output()
+    test_gqa_mha()
+    test_gqa_mqa()
+
+    for attn_type in ["mha", "mqa", "gqa"]:
+        print(f"\nRun kv cache test with {attn_type}")
+        test_kv_cache(attn_type=attn_type)
