c3d4941490df41f669c4e464b292f18189676a97
Fatescript <wangfeng19950315@163.com>
2025-01-24 21:08:22 +0800

    feat(nn): add flash attn v2

--- nn/module/flash_attn.py
+++ nn/module/flash_attn.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 # A numpy version implementation of Flash Attention
-# reference paper: https://arxiv.org/abs/2205.14135
+# reference paper:
+# v1: https://arxiv.org/abs/2205.14135
+# v2: https://arxiv.org/abs/2307.08691
 
 import itertools
 import os
@@ -107,6 +109,58 @@ class FlashAttnV1:
         return np.concatenate(outputs, axis=-2)
 
 
+class FlashAttnV2(FlashAttnV1):
+
+    def flash_attn(self, q, k, v, q_block_size: int = 2, kv_block_size: int = 1):
+        batch_size = q.shape[0]
+        num_query = q.shape[-2] // q_block_size
+        num_kv = k.shape[-2] // kv_block_size
+        k_t = np.swapaxes(k, -1, -2)
+        split_q = np.split(q, num_query, axis=-2)
+        split_k = np.split(k_t, num_kv, axis=-1)
+        split_v = np.split(v, num_kv, axis=-2)
+        attn_scalar = np.sqrt(q.shape[-1])
+
+        init_shape = (batch_size, self.num_heads, q_block_size, 1)
+        outputs = []
+
+        # Q vs K/V order swapping is not crucial
+        for local_q in split_q:  # outer loop of query, updated in v2
+            # init O (outputs), l (exp_sum), and m (row_max)
+            output = np.zeros_like(local_q)
+            exp_sum = np.zeros(init_shape)
+            row_max = np.ones(init_shape) * float("-inf")
+
+            for local_k, local_v in zip(split_k, split_v):  # key/value loop, updated in v2
+                s_ij = local_q @ local_k / attn_scalar
+                local_max = np.max(s_ij, axis=-1, keepdims=True)
+                update_max = np.maximum(row_max, local_max)
+                # NOTE: v2 removed the current_scalar
+                history_scalar = np.exp(row_max - update_max)
+
+                p_ij = np.exp(s_ij - update_max)
+                local_exp_sum = np.sum(p_ij, axis=-1, keepdims=True)
+
+                # compute output
+                o_ij = p_ij @ local_v
+                update_outputs = history_scalar * output + o_ij  # updated in v2
+                update_exp_sum = history_scalar * exp_sum + local_exp_sum
+
+                # update
+                row_max = update_max
+                exp_sum = update_exp_sum
+                output = update_outputs
+
+            # `l = row_max + np.log(exp_sum)` is needed in backward, ignore it here
+            # during backward exp(s_ij - l) = exp(s_ij - max) / exp(log(exp_sum)),
+            # which is exp(s_ij - max) / exp_sum, which is attn prob matrix
+            diag_inverse = vec_diag(1 / exp_sum.squeeze(-1))
+            output = diag_inverse @ output
+            outputs.append(output)
+
+        return np.concatenate(outputs, axis=-2)
+
+
 def flash_attn_load_state_dict(module, state_dict):
     module.q_proj = state_dict["q_proj.weight"].numpy()
     module.k_proj = state_dict["k_proj.weight"].numpy()
@@ -115,7 +169,7 @@ def flash_attn_load_state_dict(module, state_dict):
     return module
 
 
-def check_np_flash_attn_v1():
+def check_np_flash_attn(version: str = "v1"):
     # load from torch model, see experiments/nn/module/mha.py
     load_file = "mha_input_output.pkl"
     if not os.path.exists(load_file):
@@ -123,7 +177,13 @@ def check_np_flash_attn_v1():
 
     with open(load_file, "rb") as f:
         data_dict = pickle.load(f)
-    mha = FlashAttnV1(32, num_heads=4)
+    if version == "v1":
+        mha = FlashAttnV1(32, num_heads=4)
+    elif version == "v2":
+        mha = FlashAttnV2(32, num_heads=4)
+    else:
+        raise ValueError(f"Invalid version {version}, should be v1 or v2")
+
     mha = flash_attn_load_state_dict(mha, data_dict["model_state"])
 
     input_data = data_dict["input"].detach().numpy()
@@ -132,12 +192,14 @@ def check_np_flash_attn_v1():
     q_block_size = [1, 2, 4, 8, 16]
     kv_block_size = [1, 2, 4, 8, 16]
 
+    print(f"\nCheck Flash Attn {version} forward")
     for q_size, kv_size in itertools.product(q_block_size, kv_block_size):
         output = mha(input_data, q_block_size=q_size, kv_block_size=kv_size)
         match = np.allclose(output_data, output, atol=1e-7)
         result = "passed" if match else "failed"
-        print(f"Check Flash Attn v1 forward, q: {q_size:2d}, kv: {kv_size:2d}, result: {result}")
+        print(f"Check finish, q: {q_size:2d}, kv: {kv_size:2d}, result: {result}")
 
 
 if __name__ == "__main__":
-    check_np_flash_attn_v1()
+    check_np_flash_attn(version="v1")
+    check_np_flash_attn(version="v2")
